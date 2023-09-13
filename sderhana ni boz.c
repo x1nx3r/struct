ab16fb32d743bb9bbf5943627868b904950ebe1c
x1nx3r <monmega110@gmail.com>
2023-09-13 08:29:59 +0700

    stag

--- sderhana ni boz.c
+++ sderhana ni boz.c
@@ -8,10 +8,15 @@ int kali(int x, int y){
     return x*y;
 }
 
+int kurang(int x, int y){
+    return x-y;
+}
+
 int main (){
 
     printf("3*5 adalah : %d\n", kali(3,5));
-    printf("7+7 adalah : %d", tambah(7,7));
+    printf("7+7 adalah : %d\n", tambah(7,7));
+    printf("1-6 adalah : %d", kurang(1,6));
 
     return 0;
 }
