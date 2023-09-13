32203693f7072ef2d23b9701c360c3dc252590e5
x1nx3r <monmega110@gmail.com>
2023-09-13 08:32:04 +0700

    commits

---
+++ sderhana.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int tambah(int x, int y){
+    return x+y;
+}
+
+int kali(int x, int y){
+    return x*y;
+}
+
+int kurang(int x, int y){
+    return x-y;
+}
+
+int main (){
+
+    printf("3*5 adalah : %d\n", kali(3,5));
+    printf("7+7 adalah : %d\n", tambah(7,7));
+    printf("1-6 adalah : %d", kurang(1,6));
+
+    return 0;
+}
