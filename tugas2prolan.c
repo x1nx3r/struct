5a04f27e013f678ae1fb55846d88d7707a67058e
x1nx3r <monmega110@gmail.com>
2023-09-19 20:26:30 +0700

    doin tugas2

---
+++ tugas2prolan.c
@@ -0,0 +1,138 @@
+/*
+
+    Nama : Muhammad Mega Nugraha
+    NPM : 22081010213
+
+    TUGAS 2 Pemrograman Lanjut
+    Soal :
+    1. fungsi perpangkatan (void dan return value)
+    2. fungsi penggabung nama depan dan nama belakang (void dan return value)
+    3. fungsi cek validitas sebuah password: jika syarat password harus mengandung karakter ! dan minimal 8 karakter (void dan return value)
+*/
+
+#include<stdio.h>   
+#include<string.h>  //Module untuk fungsi manipulasi string
+#include<stdlib.h> 
+#include<stdbool.h> //Module untuk fungsi manipulasi boolean
+
+
+//  Soal 1 : Perpangkatan dua angka
+
+// Fungsi perpangkatan dengan return value
+int perpangkatan(int angka, int pangkat){
+    int temp=1;
+    for (int i=1;i<=pangkat;i++){
+        temp=temp*angka;
+    }
+    return temp;
+}
+
+// Fungsi perpangkatan void
+void perpangkatan_void(int angka, int pangkat){
+    int temp=1;
+    for (int i=1;i<=pangkat;i++){
+        temp=temp*angka;
+    }
+    printf("\nVoid_%d\n", temp);
+}
+
+
+// SOAL 2 : Fungsi menggabung nama depan dan belakang
+
+// Fungsi gabung nama dengan return
+char* gabung_nama(const char* depan, const char* belakang){
+    //hitung dulu total panjang keduanya
+    int totalLen = strlen(depan) + strlen(belakang) + 1; // +1 adalah untuk null terminator
+
+    //alokasi memori untuk string return
+    char* digabung = (char*)malloc(totalLen*sizeof(char));
+    if (digabung == NULL){
+        perror("Alokasi memori gagal");
+        exit(1);
+    }
+    
+    strcpy(digabung, depan);
+    strcat(digabung, " ");
+    strcat(digabung, belakang);
+    
+    return digabung;
+}
+
+// Fungsi gabung nama dengan void
+void gabung_nama_void(const char* depan, const char* belakang){
+    //hitung dulu total panjang keduanya
+    int totalLen = strlen(depan) + strlen(belakang) + 1; // +1 adalah untuk null terminator
+
+    //alokasi memori untuk string return
+    char* digabung = (char*)malloc(totalLen*sizeof(char));
+    if (digabung == NULL){
+        perror("Alokasi memori gagal");
+        exit(1);
+    }
+    
+    strcpy(digabung, depan);
+    strcat(digabung, " ");
+    strcat(digabung, belakang);
+    
+    printf("void_%s", digabung);
+}
+
+
+// SOAL 3 : Fungsi cek password
+// Fungsi cek password dengan return value
+bool cek_password(const char* passwd){
+    if (strlen(passwd) > 8){
+        if (strchr(passwd, '!') != NULL){
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
+
+// Fungsi cek password dengan void
+void cek_password_void(const char* passwd){
+    int temp = 0;
+    if (strlen(passwd) > 8){
+        if (strchr(passwd, '!') != NULL){
+            printf("Void_Password baik");
+            exit(0);
+        }
+        printf("Void_Pasword kurang baik");
+        exit(0);
+    }
+    printf("Void_Password kurang baik");
+}
+
+int main(){
+    //SOAL 1
+    //implementasi fungsi perpangkatan dengan return value
+    printf("%d", perpangkatan(2,8));
+    //implementasi fungsi perpangkatan void
+    perpangkatan_void(2,8);
+
+    //SOAL 2
+    //Inisialisasi nama depan dan belakang
+    const char* dpn = "Slamet";
+    const char* blkng = "Kopling";
+    //implementasi fungsi gabung nama dengan return value
+    printf("%s\n", gabung_nama(dpn, blkng));
+    //implementasi fungsi gabung nama void
+    gabung_nama_void(dpn, blkng);
+
+    //SOAL 3
+    //inisialisasi string(char pointer) dan memasukkan password
+    const char* passwd;
+    printf("\n(Password harus mengandung '!' dan lebih dari 8 char) input passwd :");
+    scanf("%s",passwd);
+    //Implementasi fungsi cek password dengan return value
+    if (cek_password(passwd) == true){
+        printf("Password baik!\n");
+    } else {
+        printf("Password kurang baik\n");
+    };
+    //implementasi fungsi cek password void
+    cek_password_void(passwd);
+
+    return 0;
+}
